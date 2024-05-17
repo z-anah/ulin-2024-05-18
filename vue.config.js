9634c3c2d7a7ea7b8470a5279320e28d5f6b2d4e
z-anah <40556052+z-anah@users.noreply.github.com>
2024-05-17 21:08:52 +0700

    test

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath: "",
-  publicPath: "/ulin-2024-05-18/",
+  publicPath: "",
+  // publicPath: "/ulin-2024-05-18/",
   transpileDependencies: true,
 });
